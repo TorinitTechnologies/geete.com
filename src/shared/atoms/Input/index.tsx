59c5238c8b2653ab487ec8421e8095c606e8b7b0
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2023-07-09 11:35:12 +0530

    fix: linitg

--- src/shared/atoms/Input/index.tsx
+++ src/shared/atoms/Input/index.tsx
@@ -2,7 +2,6 @@ import { StyledTextField } from './styles';
 
 function Input(props: InputProps) {
 	const { id, name, label, variant, dataTestId, type, inputProps } = props;
-	console.log('hi');
 	return (
 		<StyledTextField
 			id={id}
