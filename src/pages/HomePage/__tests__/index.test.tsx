b4d567f0d2aeb23410ba3d26d2669f87931496f4
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2023-07-13 03:02:38 +0530

    test: snapshot added

--- src/pages/HomePage/__tests__/index.test.tsx
+++ src/pages/HomePage/__tests__/index.test.tsx
@@ -1,7 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import HomePage from '../index';
 
 describe('HomePage', () => {
+	it('renders correctly', () => {
+		const tree = renderer.create(<HomePage />).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
 	it('HomePage should render', async () => {
 		render(<HomePage />);
 		const homePage = await screen.findByTestId('home_page');
