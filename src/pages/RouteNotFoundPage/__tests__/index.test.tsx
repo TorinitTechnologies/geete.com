7cabda0513eec3aec6ff9f5461b904e0f399617b
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2023-07-13 03:03:48 +0530

    test: snapshot added

--- src/pages/RouteNotFoundPage/__tests__/index.test.tsx
+++ src/pages/RouteNotFoundPage/__tests__/index.test.tsx
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import RouteNotFoundPage from '..';
 
 describe('Page Not Found Tests', () => {
+	it('renders correctly', () => {
+		const tree = renderer.create(<RouteNotFoundPage />).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
 	it('should render Page Not Found page', async () => {
 		render(<RouteNotFoundPage />);
 		const pageNotFoundElement = await screen.getByTestId('page_not_found');
